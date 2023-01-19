1b71ebda2924dfad541a1afba1a588cf12a8663e
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-19 16:34:55 +0100

    fatte le view

---
+++ resources/views/museum/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>show</h1>
+    {{-- <div class="container">
+
+        <div class="card" style="width: 18rem;">
+            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ $comic['title'] }}</h5>
+                <p class="card-text">{{ $comic['description'] }}</p>
+            </div>
+        </div>
+        <a class="btn btn-info" href="{{ route('comics.index') }}">Torna all'elenco dei comics</a>
+    </div> --}}
+@endsection
