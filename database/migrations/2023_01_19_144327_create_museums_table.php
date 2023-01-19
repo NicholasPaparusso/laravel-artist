48873de0cf1885372f4d082cf590410e0173ba05
Mattheoo89 <matteobellomo.dev@gmail.com>
2023-01-19 16:22:53 +0100

    [add] migrate + seeder

--- database/migrations/2023_01_19_144327_create_museums_table.php
+++ database/migrations/2023_01_19_144327_create_museums_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('museums', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('slug', 80);
+            $table->string('nation', 50);
             $table->timestamps();
         });
     }
--- database/seeders/MuseumsSeeder.php
+++ database/seeders/MuseumsSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Museums;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use PSpell\Config;
 
 class MuseumsSeeder extends Seeder
 {
@@ -14,6 +16,16 @@ class MuseumsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $array_Museums = Config('paintings.museums');
+        // dd($array_Museums);
+
+        foreach($array_Museums as $Museum_item){
+            
+            $new_Museums = new Museums();
+            $new_Museums->name = $Museum_item['name'];
+            $new_Museums->slug = Museums::generateSlug($new_Museums->name);
+            $new_Museums->nation = $Museum_item['nation'];
+            $new_Museums->save();
+        }
     }
 }
